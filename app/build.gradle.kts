c57a71f63b246c12e1175ca03e4ba07935c798f0
jntm <262448350@qq.com>
2023-12-08 02:10:18 +0800

    min sdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.android.toyvpn"
-        minSdk = 24
+        minSdk = 29
         targetSdk = 33
         versionCode = 1
         versionName = "1.0"
